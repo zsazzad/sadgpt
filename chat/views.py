bba6d829f8767f1f2a7bd29b8128bda6d253f34e
Obaidur Rahman <thetutorlab360@gmail.com>
2023-02-16 20:02:32 +0600

    Added feature to generate random number of guau strings

--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+import random
 
 
 # Create your views here.
@@ -14,5 +15,9 @@ def chatview(request):
 def chatgpt(request):
     if request.method == "POST":
         message = json.loads(request.body)["message"]
-        response = "This is the response from ChatGPT."
-        return JsonResponse({"message": response})
+        num_string = random.randint(1,10)
+        guau_string = ["Guau "]
+        for i in range(num_string):
+            guau_string.append("guau")
+        guau_string = " ".join(guau_string)
+        return JsonResponse({"message": guau_string})
